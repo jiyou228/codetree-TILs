f5a4849209fe5c374cd8880b88681c1131314c45
jiyou228 <92305322+jiyou228@users.noreply.github.com>
2024-11-06 20:55:30 +0900

    Title: 실수 입력받아 계산, Time: 140ms, Memory: 11MB, Status: Passed - CodeTree

--- 241106/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 241106/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double a = sc.nextInt();
-        double b = sc.nextInt();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
 
         double c = a + b;
 
