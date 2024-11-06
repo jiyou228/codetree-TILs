27a7f5411ec03a3d1fb0b56d0f8a83f093e3b747
jiyou228 <92305322+jiyou228@users.noreply.github.com>
2024-11-06 17:38:49 +0900

    Title: 합을 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241106/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+        x = a + b + c;
+
+        a = b = c = x;
+
+        System.out.print(a + " " + b + " " + c);
+    }
+}
