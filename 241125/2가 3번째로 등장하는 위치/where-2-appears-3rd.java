0b2f370a914bc2a2972f4a24d1304695d78afc3b
jiyou228 <92305322+jiyou228@users.noreply.github.com>
2024-11-25 15:25:34 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241125/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int cnt = 0;
+        int idx = -1;
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 2){
+                cnt++;
+                if(cnt == 3){
+                idx = i;
+            }
+            }
+            
+            
+        }
+        System.out.print(idx+1);
+    }
+}
